a0effa64e19809fd87cd75e9f32d5c4b43ef8609
Peffern <agoodisman@huntsman-ve565-1790.apn.wlan.upenn.edu>
2017-02-17 12:33:38 -0500

    code cleanup - official version 1.1

--- src/main/java/com/peffern/ieplants/IEPlants.java
+++ src/main/java/com/peffern/ieplants/IEPlants.java
@@ -36,7 +36,7 @@ public class IEPlants
 	public static final String MODNAME = "IE Plants";
 	
 	/** Mod Version */
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	
 	/**
 	 * Do all the main mod setup
@@ -44,11 +44,13 @@ public class IEPlants
 	@EventHandler
 	public void init(FMLInitializationEvent e)
 	{		
+		//we use our own icons since the IE ones are too small (16x16), need 32x32
 		String[] iconNames = new String[6];
 		for(int i = 1; i < iconNames.length+1; i++)
 		{
 			iconNames[i-1] = IEPlants.MODID + ":" + "Hemp (" + i + ")";
 		}
+		//TFC Crops crop generation
 		ICrop p = new BaseCrop("hemp", 1, 28, iconNames, 10, 5, 1.0f, new ItemStack(IEContent.itemMaterial,4,3), "seedsHemp", com.bioxx.tfc.Reference.MOD_ID + ":" + "food/unused/img141", "Seeds IE_Hemp")
 		{
 			@Override
@@ -59,13 +61,7 @@ public class IEPlants
 				return true;
 			}
 		};
+		//register
 		hempSeeds = CropsRegistry.addCrop(p);
-	
-		//I just realized that IE 0.7.7 doesn't use hempFiber in the oreDict
-		/*IERecipes.addShapelessOredictRecipe(new ItemStack(Items.string), "fiberHemp","fiberHemp","fiberHemp");
-		IERecipes.addOredictRecipe(new ItemStack(IEContent.itemMaterial,1,4), "HHH","HSH","HHH", 'H',"fiberHemp", 'S',"stickWood");
-		IERecipes.addOredictRecipe(new ItemStack(IEContent.itemWireCoil,4,3), " I ","ISI"," I ", 'I',"fiberHemp", 'S',"stickWood");
-		IERecipes.addOredictRecipe(new ItemStack(IEContent.blockStoneDecoration, 6, 0), "CCC", "HHH", "CCC", 'C', "lumpClay", 'H', "fiberHemp");
-		*/
 	}
 }
