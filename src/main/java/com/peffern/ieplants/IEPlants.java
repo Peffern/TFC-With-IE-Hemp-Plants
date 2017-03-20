707afbe5348f46ec1e666aa111b718e076b5d8c7
Peffern <agoodisman@hntvlan569.3194.wlan.wireless-resnet.upenn.edu>
2017-03-20 14:05:51 -0400

    no longer impersonates TerraFirmaPumpkins and screws with the ASM

--- src/main/java/com/peffern/ieplants/IEPlants.java
+++ src/main/java/com/peffern/ieplants/IEPlants.java
@@ -32,7 +32,7 @@ public class IEPlants
 	public static final String MODNAME = "IE Plants";
 	
 	/** Mod Version */
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	
 	/**
 	 * Do all the main mod setup
